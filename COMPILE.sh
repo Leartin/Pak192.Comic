7165a90517c5565f66ec7af96c3c214f47c66ae1
Leartin <leartin.dialonis@gmail.com>
2020-01-13 22:39:53 +0100

    Update COMPILE.sh

--- COMPILE.sh
+++ COMPILE.sh
@@ -118,7 +118,7 @@ compile() {
 
 echo -n 'Checking for makeobj... '
 
-if [ ! -f 'makeobj' ]; then
+if [ ! -f 'makeobj-makeobj.o' ]; then
     echo 'ERROR: makeobj not found in root folder.'
     exit 1
 fi
