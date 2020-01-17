4760e71404fbfd985bbc09a85f3d5994dc7bafef
Leartin <leartin.dialonis@gmail.com>
2020-01-17 18:15:10 +0100

    Update COMPILE_THEME.sh

--- COMPILE_THEME.sh
+++ COMPILE_THEME.sh
@@ -18,8 +18,8 @@ compile() {
 	for x in $3; do
 		if [ -d ${x} ]
 		then
-			echo ./makeobj pak$1 trunk/$x.pak $x/theme.dat
            ./makeobj pak$1 test.pak $x/theme.dat
+	   mv test.pak trunk/$x
 		else
 			cp -r $x trunk
 		fi
