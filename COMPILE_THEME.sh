7a78e5bd21c180d6ade84304609229cd9c3f0898
Leartin <leartin.dialonis@gmail.com>
2020-01-18 10:30:57 +0100

    Update COMPILE_THEME.sh

--- COMPILE_THEME.sh
+++ COMPILE_THEME.sh
@@ -4,27 +4,7 @@ echo 'pak192.comic open-source repository compiler for Linux'
 echo -e '======================================================\n'
 
 echo 'This bash compiles the theme into a new folder'
-echo -e 'called trunk/theme, makeobj must be in root folder.\n'
-
-# Does all the heavy work
-# @param $1 pakset size
-# @param $2 log message
-# @param $* list of files to compile
-compile() {
-    echo '------------------------------------------------------'
-    echo -e "Compiling $2...\n"
-	
-	
-	for x in $3; do
-		if [ -d ${x} ]
-		then
-           ./makeobj pak$1 test.pak $x/theme.dat
-	   mv -v test.pak trunk/$x.pak
-		else
-			cp -r $x trunk/theme/
-		fi
-	done
-}
+echo -e 'called themes, makeobj must be in root folder.\n'
 
 echo -n 'Checking for makeobj... '
 
@@ -35,17 +15,12 @@ fi
 
 echo -e 'OK\n'
 
-mkdir trunk
-cd trunk
-mkdir theme
-cd ..
 mkdir themes
-
-
-compile '64' 'themes' 'theme/*'
-cp -r -v ./trunk/theme /themes
+./makeobj pak64 themes/pak192comic.pak /theme/pak192comic/theme.dat
+./makeobj pak64 themes/pak192comicxxl.pak /theme/pak192comicxxl/theme.dat
+cp -v ./theme/*.tab ./themes/ 
 zip themes.zip themes
-echo dir
+
 
 echo '======================================================'
 echo 'theme complete!'
